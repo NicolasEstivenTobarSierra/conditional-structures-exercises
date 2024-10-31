106d4b17f25dfd96ef402898afb89fdb793c845e
NicolasEstivenTobarSierra <186078984+NicolasEstivenTobarSierra@users.noreply.github.com>
2024-10-31 11:21:40 -0500

    ejercicio 5.2

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-# Solicitar al usuario que ingrese dos números
+# Solicitar al usuario que ingrese tres números
 numero1 = int(input("Ingrese número: "))
 numero2 = int(input("Ingrese número: "))
+numero3 = int(input("Ingrese número: "))
 
 # Ordenar y mostrar los números
-if numero1 < numero2:
-    print(numero1, numero2)
+if numero1 <= numero2 and numero1 <= numero3:
+    if numero2 <= numero3:
+        print(numero1, numero2, numero3)
+    else:
+        print(numero1, numero3, numero2)
+elif numero2 <= numero1 and numero2 <= numero3:
+    if numero1 <= numero3:
+        print(numero2, numero1, numero3)
+    else:
+        print(numero2, numero3, numero1)
 else:
-    print(numero2, numero1)
+    if numero1 <= numero2:
+        print(numero3, numero1, numero2)
+    else:
+        print(numero3, numero2, numero1)
