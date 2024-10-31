5c6ce9c2947a5778895c53934194e0fb151d7b02
NicolasEstivenTobarSierra <186078984+NicolasEstivenTobarSierra@users.noreply.github.com>
2024-10-31 11:13:05 -0500

    ejercicio 4.0

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-# Solicitar al usuario que ingrese el dividendo y el divisor
-dividendo = int(input("Dividendo: "))
-divisor = int(input("Divisor: "))
+# Solicitar al usuario que ingrese dos palabras
+palabra1 = input("Palabra 1: ")
+palabra2 = input("Palabra 2: ")
 
-# Realizar la división
-cociente = dividendo // divisor
-resto = dividendo % divisor
+# Calcular la longitud de cada palabra
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
 
-# Determinar si la división es exacta
-if resto == 0:
-    print("La división es exacta.")
-    print(f"Cociente: {cociente}")
-    print(f"Resto: {resto}")
+# Comparar las longitudes
+if longitud1 > longitud2:
+    diferencia = longitud1 - longitud2
+    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
+elif longitud2 > longitud1:
+    diferencia = longitud2 - longitud1
+    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
 else:
-    print("La división no es exacta.")
-    print(f"Cociente: {cociente}")
-    print(f"Resto: {resto}")
+    print("Las dos palabras tienen el mismo largo.")
