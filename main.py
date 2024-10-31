6ba30a1dfcc4c98ddd011c823652b46f7fd47220
NicolasEstivenTobarSierra <186078984+NicolasEstivenTobarSierra@users.noreply.github.com>
2024-10-31 10:56:22 -0500

    Ejercicio 1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario que ingrese un número entero
+numero = int(input("Ingrese un número: "))
+
+# Verificar si el número es par o impar
+if numero % 2 == 0:
+    print("Su número es par")
+else:
+    print("Su número es impar")
